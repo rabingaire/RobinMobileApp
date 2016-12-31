e0683eb2d2bfafb730e970d7e54c4a791a34a692
Rabin Gaire <rabingaire20@gmail.com>
2016-12-31 23:58:50 +0545

    added code for ios also

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,20 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+//importing the required files
+import React from 'react';
+import { AppRegistry, Text, View } from 'react-native';
+import Header from './src/components/Header';
+import MovieList from './src/components/MovieList';
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-export default class Robin extends Component {
+//Component
+export default class MyApp extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View>
+        <Header headerText={'Robin'} />
+        <MovieList />
       </View>
-    );
+    )
   }
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('Robin', () => Robin);
+//rendering the code to device
+AppRegistry.registerComponent( 'Robin', ()=> MyApp)
